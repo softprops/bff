718f3c719248e9ab19f15453175640bfd7900f85
softprops <d.tangren@gmail.com>
2011-07-14 02:50:04 -0400

    better name

--- src/main/scala/bff.scala
+++ src/main/scala/bff.scala
@@ -6,22 +6,22 @@ object @#^* {
   def apply[T](in: => Int, out: Int => T)(src: String) =
     src.toArray match {
       case cmds =>
-        @annotation.tailrec def next(state: List[Int], ls: List[(Int, Int)], dp: Int, ip: Int): Unit =
+        @annotation.tailrec def next(cells: List[Int], ls: List[(Int, Int)], dp: Int, ip: Int): Unit =
           if(ip < cmds.size) {
             cmds(ip) match {
-              case '<' => next(state, ls, dp - 1, ip + 1)
-              case '>' => next(state, ls, dp + 1, ip + 1)
-              case '+' => next(state.updated(dp, state(dp) + 1), ls, dp, ip + 1)
-              case '-' => next(state.updated(dp, state(dp) - 1), ls, dp, ip + 1)
-              case '.' => out(state(dp)); next(state, ls, dp, ip + 1)
-              case ',' => next(state.updated(dp, in), ls, dp, ip + 1)
+              case '<' => next(cells, ls, dp - 1, ip + 1)
+              case '>' => next(cells, ls, dp + 1, ip + 1)
+              case '+' => next(cells.updated(dp, cells(dp) + 1), ls, dp, ip + 1)
+              case '-' => next(cells.updated(dp, cells(dp) - 1), ls, dp, ip + 1)
+              case '.' => out(cells(dp)); next(cells, ls, dp, ip + 1)
+              case ',' => next(cells.updated(dp, in), ls, dp, ip + 1)
               case '[' =>
-                if(state(dp) == 0) next(state, ls.init, dp, ls.last._2 + 1)
-                else next(state, ls :+ (ip, 0), dp, ip + 1)
+                if(cells(dp) == 0) next(cells, ls.init, dp, ls.last._2 + 1)
+                else next(cells, ls :+ (ip, 0), dp, ip + 1)
               case ']' =>
-                if(state(dp) != 0) next(state, ls.updated(ls.size - 1, (ls.last._1, ip)), dp, ls.last._1 + 1)
-                else next(state, ls, dp, ip + 1)
-              case _ => next(state, ls, dp, ip)
+                if(cells(dp) != 0) next(cells, ls.updated(ls.size - 1, (ls.last._1, ip)), dp, ls.last._1 + 1)
+                else next(cells, ls, dp, ip + 1)
+              case _ => next(cells, ls, dp, ip)
             }
           }
         next(List.fill(30000)(0), Nil, 0, 0)
